c04631aad144dfd8e3a53af62d06020bc2258828
Socvest <82335390+Socvest@users.noreply.github.com>
2023-10-09 10:58:46 +0100

    Create dummy-page.py

---
+++ pages/dummy-page.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from streamlit_custom_sidebar import CustomSidebarDefault
+import streamlit_float # recommended
+
+
+streamlit_float.float_init()
+
+data_ = [
+            {"index":0, "label":"Example", "page":"example", "href":"http://localhost:8501/"},
+            {"index":1, "label":"Page", "page":"page", "icon":"ri-logout-box-r-line", "href":"http://localhost:8501/page"}
+        ]
+
+if "currentPage" not in st.session_state: # required as component will be looking for this in session state to change page via `switch_page`
+    st.session_state["currentPage"] = data_[0] 
+else:
+    st.session_state["currentPage"] = data_[0] 
+
+
+with st.container():
+    defaultSidebar = CustomSidebarDefault(closeNavOnLoad=False, backgroundColor="brown", loadPageName="example", data=data_, LocalOrSessionStorage=1, serverRendering=False, webMedium="local") 
+    defaultSidebar.load_custom_sidebar()
+    defaultSidebar.change_page()
+    
+    streamlit_float.float_parent(css="top:-1000px;")
