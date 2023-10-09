512ee6e576e24eb34f337a76f5f233b7e3b4bad8
Socvest <82335390+Socvest@users.noreply.github.com>
2023-10-09 11:41:14 +0100

    Update dummy-page.py

--- pages/dummy-page.py
+++ pages/dummy-page.py
@@ -2,14 +2,14 @@ import streamlit as st
 from streamlit_custom_sidebar import CustomSidebarDefault
 import streamlit_float # recommended
 
-st.set_page_config(layout="wide", page_title="dummpy-page")
+st.set_page_config(layout="wide", page_title="dummy-page")
 
 
 streamlit_float.float_init()
 
 data_ = [
-            {"index":0, "label":"Home Page", "page":"home-page", "href":"http://localhost:8501/"},
-            {"index":1, "label":"Dummy Page", "page":"dummy-page", "icon":"ri-logout-box-r-line", "href":"http://localhost:8501/dummy-page"}
+            {"index":0, "label":"Home Page", "page":"home-page", "href":"https://custom-sidebar.streamlit.app/"},
+            {"index":1, "label":"Dummy Page", "page":"dummy-page", "icon":"ri-logout-box-r-line", "href":"https://custom-sidebar.streamlit.app/dummy-page"}
         ]
 
 if "currentPage" not in st.session_state: # required as component will be looking for this in session state to change page via `switch_page`
